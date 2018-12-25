622a81fea240f9099f198533dd037deb9715d7cc
Emrah TURAN <mail.emrahturan@gmail.com>
2018-12-25 12:41:01 +0300

    Create Project

---
+++ MyLibrary/MainForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace MyLibrary
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbAuthors = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.btnAuthorSave = new System.Windows.Forms.Button();
+            this.btnAuthorDelete = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbCategories = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.btnCategoryDelete = new System.Windows.Forms.Button();
+            this.btnCategorySave = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbPublishers = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAuthorDelete);
+            this.groupBox1.Controls.Add(this.btnAuthorSave);
+            this.groupBox1.Controls.Add(this.txtFirstName);
+            this.groupBox1.Controls.Add(this.cmbAuthors);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 132);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Author Operations";
+            // 
+            // cmbAuthors
+            // 
+            this.cmbAuthors.FormattingEnabled = true;
+            this.cmbAuthors.Location = new System.Drawing.Point(6, 19);
+            this.cmbAuthors.Name = "cmbAuthors";
+            this.cmbAuthors.Size = new System.Drawing.Size(238, 21);
+            this.cmbAuthors.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Full Name:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(72, 57);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(172, 20);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // btnAuthorSave
+            // 
+            this.btnAuthorSave.Location = new System.Drawing.Point(159, 91);
+            this.btnAuthorSave.Name = "btnAuthorSave";
+            this.btnAuthorSave.Size = new System.Drawing.Size(85, 23);
+            this.btnAuthorSave.TabIndex = 1;
+            this.btnAuthorSave.Text = "Save";
+            this.btnAuthorSave.UseVisualStyleBackColor = true;
+            // 
+            // btnAuthorDelete
+            // 
+            this.btnAuthorDelete.Location = new System.Drawing.Point(72, 91);
+            this.btnAuthorDelete.Name = "btnAuthorDelete";
+            this.btnAuthorDelete.Size = new System.Drawing.Size(85, 23);
+            this.btnAuthorDelete.TabIndex = 1;
+            this.btnAuthorDelete.Text = "Delete";
+            this.btnAuthorDelete.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCategoryDelete);
+            this.groupBox2.Controls.Add(this.txtCategory);
+            this.groupBox2.Controls.Add(this.btnCategorySave);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cmbCategories);
+            this.groupBox2.Location = new System.Drawing.Point(268, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 132);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Category Operations";
+            // 
+            // cmbCategories
+            // 
+            this.cmbCategories.FormattingEnabled = true;
+            this.cmbCategories.Location = new System.Drawing.Point(6, 19);
+            this.cmbCategories.Name = "cmbCategories";
+            this.cmbCategories.Size = new System.Drawing.Size(238, 21);
+            this.cmbCategories.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Category:";
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(72, 57);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(172, 20);
+            this.txtCategory.TabIndex = 2;
+            // 
+            // btnCategoryDelete
+            // 
+            this.btnCategoryDelete.Location = new System.Drawing.Point(72, 91);
+            this.btnCategoryDelete.Name = "btnCategoryDelete";
+            this.btnCategoryDelete.Size = new System.Drawing.Size(85, 23);
+            this.btnCategoryDelete.TabIndex = 3;
+            this.btnCategoryDelete.Text = "Delete";
+            this.btnCategoryDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCategorySave
+            // 
+            this.btnCategorySave.Location = new System.Drawing.Point(159, 91);
+            this.btnCategorySave.Name = "btnCategorySave";
+            this.btnCategorySave.Size = new System.Drawing.Size(85, 23);
+            this.btnCategorySave.TabIndex = 4;
+            this.btnCategorySave.Text = "Save";
+            this.btnCategorySave.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.txtPublisher);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.cmbPublishers);
+            this.groupBox3.Location = new System.Drawing.Point(524, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 132);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Publisher Operations";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.Location = new System.Drawing.Point(72, 57);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(172, 20);
+            this.txtPublisher.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Publisher:";
+            // 
+            // cmbPublishers
+            // 
+            this.cmbPublishers.FormattingEnabled = true;
+            this.cmbPublishers.Location = new System.Drawing.Point(6, 19);
+            this.cmbPublishers.Name = "cmbPublishers";
+            this.cmbPublishers.Size = new System.Drawing.Size(238, 21);
+            this.cmbPublishers.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAuthorSave;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.ComboBox cmbAuthors;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAuthorDelete;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbCategories;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCategoryDelete;
+        private System.Windows.Forms.Button btnCategorySave;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbPublishers;
+    }
+}
+
