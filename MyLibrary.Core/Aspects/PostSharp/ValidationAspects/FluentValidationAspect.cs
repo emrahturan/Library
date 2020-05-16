4be5604804d733f5ef565ee1f22d9fbc25fd94c0
Emrah Turan <emrah.turan@softtech.isbank.com.tr>
2020-05-16 12:08:36 +0300

    Core Layer - FluentValidation implemantation and add validation aspect

---
+++ MyLibrary.Core/Aspects/PostSharp/ValidationAspects/FluentValidationAspect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MyLibrary.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using PostSharp.Aspects;
+
+namespace MyLibrary.Core.Aspects.PostSharp.ValidationAspects
+{
+    [Serializable]
+    public class FluentValidationAspect : OnMethodBoundaryAspect
+    {
+        Type _validatorType;
+
+        public FluentValidationAspect(Type validatorType)
+        {
+            _validatorType = validatorType;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            //ProductValidator : AbstractValidator<Product> buradan Product'ı alıyoruz
+            if (_validatorType.BaseType != null)
+            {
+                var entityType = _validatorType.BaseType.GetGenericArguments()[0];
+                var entities = args.Arguments.Where(t => t.GetType() == entityType);
+
+                foreach (var entity in entities)
+                {
+                    ValidatorTool.Validate(validator, entity);
+                }
+            }
+        }
+    }
+}
